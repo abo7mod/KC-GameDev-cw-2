b8ab57bc07d769cd135091a587e282b24c2f5228
abo7mod <57900170+abo7mod@users.noreply.github.com>
2022-07-30 15:13:11 +0300

    بونص

--- class 2/Assets/c.cs
+++ class 2/Assets/c.cs
@@ -12,15 +12,22 @@ public class c : MonoBehaviour
     float villaninHeight = 1.71f;
     int villainAge = 18;
     string villainSuperPower = "invisibility";
+    float heroHeightafter = 1.71f + 5;
+    string villainSuperPowerafter = "None";
 
 
-    void Start()
+
+     void Start()
     {
         print("hero name is "  +  heroName +  "hero age is"  +  heroAge +  "hero Height is"  + heroHeight +  "hero Super power is "  +  heroSuperPower);
 
         print("villain name is "  + villainName +  "villain age is"  + villainAge +  "villain Height in "  +  villaninHeight  +  "villain super power is"  +  villainSuperPower);
 
         print("age Difference is " + (heroAge - villainAge));
+
+        print("hero height after is " + heroHeightafter );
+
+        print("villain Super Power after is" + villainSuperPowerafter);
         
     }
 
